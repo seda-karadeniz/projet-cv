f24b8e9ef55127b0ac63ec2bdfd10c083609c9fd
seda-karadeniz <72101144+seda-karadeniz@users.noreply.github.com>
2022-03-02 00:05:41 +0100

    change

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
     function myFunction() {
     // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
+    var input, filter, ul, li, span, i, txtValue;
     input = document.getElementById('myInput');
     filter = input.value.toUpperCase();
     ul = document.getElementById("myUL");
@@ -9,8 +9,8 @@
 
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
+    span = li[i].getElementsByTagName("span")[0];
+    txtValue = span.textContent || span.innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
     li[i].style.display = "";
 } else {
